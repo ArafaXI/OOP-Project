e5897d9498cc17263e641378ecfa832d2ddd39a7
codings1 <a1921806@student.adelaide.edu.au>
2024-10-13 19:09:07 +1030

    Item comments

--- Item.cpp
+++ Item.cpp
@@ -1,25 +1,43 @@
 #include "Item.h"
 
-Item:: Item(){ // base constructor sets empty attributes 
-    this -> title = "";
-    this -> author = "";
+// Default constructor for the Item class
+// Initializes title and author to empty strings
+Item::Item() { 
+    this->title = "";  // Set title to an empty string
+    this->author = ""; // Set author to an empty string
 }
 
-Item:: Item(std:: string title, std:: string author){
-    this -> title = title;
-    this -> author = author;
+// Parameterized constructor for the Item class
+// Allows the initialization of title and author with specific values
+Item::Item(std::string title, std::string author) { 
+    this->title = title;    // Set the title with the provided value
+    this->author = author;  // Set the author with the provided value
 }
 
-std:: string Item:: getTitle(){return this -> title;}
-
-std:: string Item:: getAuthor(){return this -> author;}
-
-void Item:: setTitle(std:: string title){
-    this -> title = title;
+// Getter for the title attribute
+// Returns the current value of the title
+std::string Item::getTitle() {
+    return this->title;
 }
 
-void Item:: setAuthor(std:: string author){
-    this -> author = author;
+// Getter for the author attribute
+// Returns the current value of the author
+std::string Item::getAuthor() {
+    return this->author;
 }
 
-Item:: ~Item() {} 
+// Setter for the title attribute
+// Sets the title with a new value
+void Item::setTitle(std::string title) {
+    this->title = title;
+}
+
+// Setter for the author attribute
+// Sets the author with a new value
+void Item::setAuthor(std::string author) {
+    this->author = author;
+}
+
+// Destructor for the Item class
+// No dynamic memory to clean up, so it is left empty
+Item::~Item() {}
--- Item.h
+++ Item.h
@@ -3,47 +3,61 @@
 
 #include <iostream>
 #include <string>
-#include <fstream> // for loading and saving
+#include <fstream> // Necessary for file operations such as loading and saving
 
-// This class will act as a parent class for the classes PrintedItem and
-// DigitalItem, and is a virtual pure class that is not meant to be
-// instantiated on its own.
+// The Item class serves as a base class for PrintedItem and DigitalItem.
+// It is a pure virtual class, meaning it is not designed to be instantiated directly.
+// It provides common attributes and functions that will be inherited and implemented by derived classes.
 
 class Item {
 private:
-    std::string title;
-    std::string author;
+    std::string title;   // Holds the title of the item
+    std::string author;  // Holds the author of the item
 
 public:
     // Default constructor
+    // Initializes title and author to empty strings
     Item();
 
     // Parameterized constructor
-    Item(std:: string title, std:: string author);
+    // Allows setting title and author at the time of object creation
+    Item(std::string title, std::string author);
 
-    // Getters
+    // Getter for the title attribute
+    // Returns the value of the title
     std::string getTitle();
 
+    // Getter for the author attribute
+    // Returns the value of the author
     std::string getAuthor();
 
-    // Setters
-    void setTitle(std:: string title);
+    // Setter for the title attribute
+    // Allows setting a new value for the title
+    void setTitle(std::string title);
 
-    void setAuthor(std:: string author);
+    // Setter for the author attribute
+    // Allows setting a new value for the author
+    void setAuthor(std::string author);
 
     // Pure virtual function to display item information
+    // This will be implemented in derived classes, forcing them to define their own version of displayInfo.
     virtual void displayInfo() = 0;
 
-    // Borrow pure abstract
+    // Pure virtual function for borrowing or returning an item
+    // This will be defined in the derived classes to handle borrowing logic.
     virtual void Borrow(bool borrow) = 0;
     
+    // Pure virtual function to check borrowing status
+    // The derived classes will implement this to return whether the item is borrowed.
     virtual bool getisBorrowed() = 0;
 
-    // For loading and saving. Every class inherited from item has different attributes to lod and save. 
-    virtual std:: string save() = 0;
-    virtual void load(std:: string& data) = 0;
+    // Pure virtual functions for saving and loading
+    // Each derived class will have its own unique attributes to save and load, so these functions will be overridden.
+    virtual std::string save() = 0;
+    virtual void load(std::string& data) = 0;
 
     // Virtual destructor
+    // Ensures that the derived class destructors are called properly when deleting an Item object.
     virtual ~Item();
 };
 
